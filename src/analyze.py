c825f1aaaaec3623a837d2f80373ca97fde06744
Masahiro Ishii <marufeuille@gmail.com>
2020-12-31 12:45:46 +0900

    write comment

--- src/analyze.py
+++ src/analyze.py
@@ -5,15 +5,27 @@ from sqlparse.tokens import DML, Keyword, Punctuation
 from sqlparse.sql import Identifier, IdentifierList, TokenList, Token
 
 def iter_subqueries(token: Token):
+    """https://blog.hoxo-m.com/entry/sqlparse_parse"""
     if token.ttype == sqlparse.tokens.DML:
         yield token.parent
-    if not isinstance(token, TokenList): # リーフノードの場合は子を探索しない
+    if not isinstance(token, TokenList):
         return
     for t in token:
         yield from iter_subqueries(t)
 
 
 def analyze_identifier(identifier: Identifier):
+    """get necesarry information from Identifier Object
+
+    Parameters
+    ----------
+    identifier : Identifier
+
+    Returns
+    -------
+    dict
+        identifier basic information
+    """
     alias = identifier.get_alias()
     real_name = identifier.get_real_name()
     if alias:
@@ -22,6 +34,18 @@ def analyze_identifier(identifier: Identifier):
         return {"name": real_name, "from": []}
 
 def analyze_select(statement:sqlparse.sql.Statement):
+    """Analyze Select Query Statement
+
+    Parameters
+    ----------
+    statement : Statement
+        Select Statement Object
+
+    Returns
+    -------
+    dict
+        analyze relult
+    """
 
     current_idx = 0
     t = statement.token_first(skip_ws=True, skip_cm=True)
@@ -30,7 +54,6 @@ def analyze_select(statement:sqlparse.sql.Statement):
     }
     from_table = None
     while t is not None:
-        print(t, t.ttype)
         if t.ttype == DML:
             current_idx, t = statement.token_next(current_idx)
             if isinstance(t, Identifier):
