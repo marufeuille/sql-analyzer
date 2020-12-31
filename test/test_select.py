8e91fd4c681eee88a7b0af8b0f7a06d2f2fedfe2
Masahiro Ishii <marufeuille@gmail.com>
2020-12-31 19:07:21 +0900

    add test case for cte and function

--- test/test_select.py
+++ test/test_select.py
@@ -1,4 +1,4 @@
-from src.analyze import analyze_select, iter_subqueries
+from src.analyze import analyze_dml, iter_subqueries
 import pytest
 import sqlparse
 
@@ -13,7 +13,7 @@ def test_simple_select():
         ]
     }
     subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
-    assert analyze_select(subqueries[0]) == expected, "single column select"
+    assert analyze_dml(subqueries[0]) == expected, "single column select"
 
     sql = "SELECT x, y FROM t0"
     expected = {
@@ -29,7 +29,7 @@ def test_simple_select():
         ]
     }
     subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
-    assert analyze_select(subqueries[0]) == expected, "multiple columns select"
+    assert analyze_dml(subqueries[0]) == expected, "multiple columns select"
 
     sql = "SELECT x AS x2 FROM t0"
     expected = {
@@ -42,7 +42,7 @@ def test_simple_select():
         ]
     }
     subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
-    assert analyze_select(subqueries[0]) == expected, "has alias column"
+    assert analyze_dml(subqueries[0]) == expected, "has alias column"
 
 def test_inference_types():
     sql = "SELECT x, y FROM t0"
@@ -66,7 +66,7 @@ def test_inference_types():
     }
 
     subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
-    assert analyze_select(subqueries[0], table_definitions=table_definitions) == expected, "inference types"
+    assert analyze_dml(subqueries[0], table_definitions=table_definitions) == expected, "inference types"
 
 
 def test_inference_asterisk():
@@ -91,7 +91,7 @@ def test_inference_asterisk():
     }
 
     subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
-    assert analyze_select(subqueries[0], table_definitions=table_definitions) == expected, "inference from static table definition"
+    assert analyze_dml(subqueries[0], table_definitions=table_definitions) == expected, "inference from static table definition"
 
     sql = "SELECT * EXCEPT(y) FROM t0"
     table_definitions = {"t0": [
@@ -109,7 +109,7 @@ def test_inference_asterisk():
     }
 
     subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
-    assert analyze_select(subqueries[0], table_definitions=table_definitions) == expected, "inference with one EXCEPT"
+    assert analyze_dml(subqueries[0], table_definitions=table_definitions) == expected, "inference with one EXCEPT"
 
 def test_inference_from_ancestor():
     table_definitions = {"t0": [
@@ -119,7 +119,7 @@ def test_inference_from_ancestor():
     sql1 = "SELECT x, y FROM t0 WHERE x > 0"
     sql1_output_tablename = "t1"
     subqueries = list(iter_subqueries(sqlparse.parse(sql1)[0]))
-    table_definitions[sql1_output_tablename] = analyze_select(subqueries[0], table_definitions=table_definitions)["COLUMNS"]
+    table_definitions[sql1_output_tablename] = analyze_dml(subqueries[0], table_definitions=table_definitions)["COLUMNS"]
     sql2 = "SELECT x, y FROM t1 WHERE y < 0"
     expected = {
         "COLUMNS": [
@@ -136,6 +136,73 @@ def test_inference_from_ancestor():
         ]
     }
 
-    print(table_definitions)
     subqueries = list(iter_subqueries(sqlparse.parse(sql2)[0]))
-    assert analyze_select(subqueries[0], table_definitions=table_definitions) == expected, "multiple inference"
+    assert analyze_dml(subqueries[0], table_definitions=table_definitions) == expected, "multiple inference"
+
+def test_cte():
+    sql = """
+    WITH t1 AS (
+        SELECT
+            a, b
+        FROM
+            t0
+    )
+    SELECT
+        a, b
+    FROM
+        t1
+    """
+    expected = {
+        "COLUMNS": [{
+            {
+                "name": "a",
+                "from": ["t1"]
+            },
+            {
+                "name": "b",
+                "from": ["t1"]
+            },
+        }],
+        "CTE": {
+            "t1": [
+                {
+                    "name": "a",
+                    "from": ["t0"]
+                },
+                {
+                    "name": "a",
+                    "from": ["t0"]
+                },
+
+            ]
+        }
+    }
+    subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
+    assert analyze_dml(subqueries[0], ) == expected, "simple cte"
+
+def test_agg_functions():
+    sql = """
+    SELECT
+        SUM(x)
+    FROM
+        t0
+    """
+    expected = {
+        "COLUMNS": [
+            {
+                "name": "no_name",
+                "type": "INTEGER",
+                "from": ["t1"],
+                "aggregator": "sum",
+                "agg_columns": ["x"]
+            },
+            {
+                "name": "y",
+                "type": "INTEGER",
+                "from": ["t1"]
+            }
+        ]
+    }
+
+    subqueries = list(iter_subqueries(sqlparse.parse(sql)[0]))
+    assert analyze_dml(subqueries[0]) == expected, "simple aggregate"
